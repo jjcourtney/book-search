d61fe738abcda479c4c45227530c82d1d54aec2f
jjcourtney <jcourtneywork@gmail.com>
2021-12-13 18:13:25 +0000

    Added flow for resolvers (To test)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,96 @@
-// import middleware
-const { authMiddleware } = require('../utils/auth');
-
-const {
-    createUser,
-    getSingleUser,
-    saveBook,
-    deleteBook,
-    login,
-} = require('../controllers/user-controller');
 
 const resolvers = {
     Query: {
-        getSingleUser,
+        getSingleUser: async (_, args, { user }) => {
+            if (user) {
+                return User.findOne({
+                    _id: user._id
+                });
+            }
+            throw new AuthenticationError("Please log in");
+        },
+    },
 
-    },
     Mutation: {
-        createUser,
-        saveBook,
-        deleteBook,
-        login,
+        createUser: async (_, { username, email, password }) => {
+
+            const newUser = await User.create({
+                username,
+                email,
+                password
+            });
+
+            const token = signToken(newUser);
+
+            return {
+                token,
+                newUser
+            };
+        },
+        saveBook: async (_, { book }, { user }) => {
+
+            if (user) {
+                return await User.findOneAndUpdate(
+                    {
+                        _id: user._id
+                    },
+                    {
+                        $addToSet: {
+                            savedBooks: book
+                        }
+                    },
+                    {
+                        new: true,
+                        runValidators: true
+                    }
+                );
+            }
+            throw new AuthenticationError("Login first to add a book!");
+        },
+        deleteBook: async (_, { bookId }, { user }) => {
+
+            if (user) {
+                return await User.findOneAndUpdate(
+                    {
+                        _id: user._id
+                    },
+                    {
+                        $pull: {
+                            savedBooks: {
+                                bookId
+                            }
+                        }
+                    },
+                    {
+                        new: true
+                    }
+                );
+            }
+            throw new AuthenticationError("Login to complete action");
+        },
     },
-};
+    login: async (_, { email, password }) => {
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new AuthenticationError("Login Failed");
+        }
+
+        const correctPassword = await user.isCorrectPassword(password);
+
+        if (!correctPassword) {
+            throw new AuthenticationError("Login Failed");
+        }
+
+        const token = signToken(user);
+
+        return {
+            token,
+            user
+        };
+    },
+}
+
 
 module.exports = resolvers;
