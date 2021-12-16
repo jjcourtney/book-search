bb747a12e5ddf3da0fada9637343eff1603a6b27
jjcourtney <jcourtneywork@gmail.com>
2021-12-16 23:22:45 +0000

    whitespace refactor

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,9 +28,23 @@ module.exports = {
     return req;
 
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({
+    username,
+    email,
+    _id
+  }) {
+    const payload = {
+      username,
+      email,
+      _id
+    };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return jwt.sign({
+      data: payload
+    },
+      secret,
+      {
+        expiresIn: expiration
+      });
   },
 };
