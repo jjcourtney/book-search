eb86984aff0e498a44280d748229240f09b8ba27
jjcourtney <jcourtneywork@gmail.com>
2021-12-16 23:20:24 +0000

    added email to User type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  input BookToSave {
+    _id: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+}
+
   type Book {
     _id: ID!
     authors: [String]!
@@ -12,6 +21,7 @@ const typeDefs = gql`
 
   type User {
     _id: ID!
+    email: String!
     username: String!
     password: String!
     savedBooks: [Book]
@@ -21,10 +31,14 @@ const typeDefs = gql`
     getSingleUser: User
   }
 
+  type LoggedInUser {
+    token: String!
+    user: User!
+  }
+
   type Mutation {
-    createUser(email: String!, password: String!, 
-    username: String!): User
-    saveBook(_id: String!, techNum: Int!): Book
+    createUser(email: String!, password: String!, username: String!): LoggedInUser
+    saveBook(_id: String!, book: BookToSave): Book
     deleteBook(_id: String!) : Book
     login(email: String!, password: String!) : User
   }
