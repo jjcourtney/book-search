9d6b974570a40395d071ba635cb3a7ef67b53cca
jjcourtney <jcourtneywork@gmail.com>
2021-12-16 23:19:38 +0000

    removed workaround for v3 apollo

--- server/server.js
+++ server/server.js
@@ -4,31 +4,30 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 const db = require("./config/connection");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req, res }) => authMiddleware(req, res)
-  })
-  const app = express();
-  await server.start();
-  server.applyMiddleware({ app, path: '/graphql' });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => authMiddleware(req, res),
+});
 
+server.applyMiddleware({ app });
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
-  })
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-startApolloServer(typeDefs, resolvers);
-
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
