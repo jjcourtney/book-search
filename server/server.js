93f396de510c5e7af2ab0f7d25796d858a58746b
jjcourtney <jcourtneywork@gmail.com>
2021-12-17 23:59:47 +0000

    heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const db = require("./config/connection");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
