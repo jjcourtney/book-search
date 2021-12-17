fa8ea481d39b4ca467c3ab0c87cabebaa23d34a2
jjcourtney <jcourtneywork@gmail.com>
2021-12-17 23:05:49 +0000

    refactored flow for bugfix

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,37 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-
-import {
-  ApolloProvider,
   ApolloClient,
   InMemoryCache,
-  HttpLink,
-  ApolloLink,
-  concat
-
-} from "@apollo/client"
-
-import Auth from "./utils/auth";
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-
-  const httpLink = new HttpLink({ uri: "/graphql" });
-  const authMiddleware = new ApolloLink((operation, forward) => {
-    // add the authorization to the headers
-    operation.setContext(({ headers = {} }) => ({
-      headers: {
-        ...headers,
-        authorization: Auth.loggedIn() ? Auth.getToken() : null,
-      },
-    }));
-
-    return forward(operation);
-  });
-
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: concat(authMiddleware, httpLink),
-  });
-
   return (
     <ApolloProvider client={client}>
       <Router>
