996708e1b6d670587c38975b5d183f7762381379
jjcourtney <jcourtneywork@gmail.com>
2021-12-18 02:00:47 +0000

    fix bug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import { useMutation } from "@apollo/client";
-import "./style.css";
-
 import Auth from "../utils/auth";
 import { SAVE_BOOK } from "../utils/queries";
 import { searchGoogleBooks } from "../utils/API";
