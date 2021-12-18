ed5e09f7fa4f9d49f071b5f6658d642b2cfc9e6b
jjcourtney <jcourtneywork@gmail.com>
2021-12-18 01:58:15 +0000

    refect after heroku deploy

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,79 +1,71 @@
-import React from 'react';
-import {
-  Jumbotron,
-  Container,
-  CardColumns,
-  Card,
-  Button
-} from 'react-bootstrap';
-import {
-  useMutation,
-  useQuery
-} from "@apollo/client"
-import Auth from '../utils/auth';
-
-import { removeBookId } from '../utils/localStorage';
-import {
-  GET_ME,
-  REMOVE_BOOK
-} from "../utils/queries"
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useMutation, useQuery } from "@apollo/client";
+import "./style.css";
+import { GET_ME, REMOVE_BOOK } from "../utils/queries";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(GET_ME);
-  const userData = data?.me || {};
-  const [removeBook] = useMutation(REMOVE_BOOK)
+
+  const { data, loading } = useQuery(GET_ME);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
 
   const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
-      const { data } = await removeBook({
-        variables: { bookId }
-      });
-
-      if (!data) {
-        throw new Error("Error fetching data")
-      }
-
+      const { data } = await deleteBook({ variables: { bookId } });
+      // upon success, remove book's id from localStorage
       removeBookId(bookId);
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      console.error(err);
     }
   };
 
-  // If data is still loading, say so
+  // if data isn't here yet, say so
   if (loading) {
-    return <h2>LOADING...</h2>;
+    return (
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    );
   }
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Viewing saved books!</h1>
+          <button className="btn-lg disabled  px-4 py-2 w-100">
+            Your favourite Books{" "}
+            <span className="badge badge-danger display-2">
+              {data.me.savedBooks.length}
+            </span>
+          </button>
         </Container>
       </Jumbotron>
       <Container>
-        <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <CardColumns>
-          {userData.savedBooks.map((book) => {
+        <CardColumns className="bg-info p-3 ">
+          {data.me.savedBooks.map((book) => {
             return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Card
+                key={book.bookId}
+                border="dark"
+                className="view overlay zoom cardBody"
+              >
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
+                  <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book!
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
+                    🗑️ Delete this Book!
                   </Button>
                 </Card.Body>
               </Card>
